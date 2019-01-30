8b9e830709673bc8c3789ce3f8e3e8a341aec278
SaulSoto <alexisalvarez@grupovidanta.com>
2019-01-29 23:13:19 -0600

    Confirmed VSCode  Debugging and Workflow

--- vscode_qlik.py
+++ vscode_qlik.py
@@ -1,4 +1,10 @@
 import pandas as pd
+import pandas as pd
 import numpy as np
 
-print('this is a test')
+df = pd.DataFrame([[1,2],[3,4]], columns=['A','B'])
+
+print(df)
+print(df.head(1))
+
+print('test')
