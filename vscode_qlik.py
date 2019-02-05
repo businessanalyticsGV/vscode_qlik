cbf4dff83d143dbc0c346b3325050c17cf54fc48
Alexis Saul Alvarez Soto <alexisalvarez@grupovidanta.com>
2019-02-05 17:42:39 -0600

    Added warning message in case Qv or VSCode aren't opened.

--- vscode_qlik.py
+++ vscode_qlik.py
@@ -1,10 +1,17 @@
 #path: C:\Users\alexisalvarez\OneDrive - Grupo Vidanta\UPDATE\Work\10. 05Feb19 - Summerfest Quickie\query.qvw
 
+### I.- 
 import psutil
+import win32api
 import numpy as np
 
-ls_OpenedWindows = [item.name() for item in psutil.process_iter() \
+var_OpenedWindows = [item.name() for item in psutil.process_iter() \
                     if 'Code.exe' in item.name() or 'Qv.exe' in item.name()]
 
-ls_OpenedWindows = np.unique(ls_OpenedWindows)
-print(len(ls_OpenedWindows))
+var_OpenedWindows = len(np.unique(var_OpenedWindows))
+
+if var_OpenedWindows != 2:
+    win32api.MessageBox(0,'VSCode y QlikView tienen que estar abiertos.','Error')
+    exit()
+
+print(var_OpenedWindows)
