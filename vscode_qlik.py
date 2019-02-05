1c42c15f5618d41fd8bdf3c150f97e171f13ab88
Alexis Saul Alvarez Soto <alexisalvarez@grupovidanta.com>
2019-02-05 16:23:46 -0600

    Added python comment to create a connection with Qlikview

--- vscode_qlik.py
+++ vscode_qlik.py
@@ -1,7 +1,10 @@
+#path: C:\Users\alexisalvarez\OneDrive - Grupo Vidanta\UPDATE\Work\10. 05Feb19 - Summerfest Quickie\query.qvw
+
 import psutil
+import numpy as np
 
 ls_OpenedWindows = [item.name() for item in psutil.process_iter() \
                     if 'Code.exe' in item.name() or 'Qv.exe' in item.name()]
 
 ls_OpenedWindows = np.unique(ls_OpenedWindows)
-len(ls_OpenedWindows)
+print(len(ls_OpenedWindows))
